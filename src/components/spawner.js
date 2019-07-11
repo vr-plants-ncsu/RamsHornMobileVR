021857c021c96cd6e0f156b7c024b796d764c9f5
vrplantsncsu <51127119+vrplantsncsu@users.noreply.github.com>
2019-07-11 14:34:02 -0400

    Refactor Spawner script
    Gave up copying A-Blast class structure for now, refactored "spawner" script to work for screen. Ideally this should be updated to provide polymorphism for scripts (i.e. scripts can override event that causes "spawning", defer "spawn" properties to spaned item rather than spawner, etc.

--- src/components/spawner.js
+++ src/components/spawner.js
@@ -6,7 +6,10 @@ AFRAME.registerComponent('spawner', {
     offset: {type: 'vec3', default: {x: 0, y: 0, z: 0}},
     rotation: {type: 'vec3', default: {x: 0, y: 0, z: 0}},
     mixin: {default: ''},
-    class: {default: 'grabbable'}
+    class: {default: 'grabbable'},
+    spawnVector: {type: 'vec3', default: {x: 0, y: 0, z: -1}},
+    spawnMagnitude: {type: 'number', default: 1.0},
+    spawnChargeTime: {type: 'number', default: 1000}
   },
 
   init: function() {
@@ -14,20 +17,32 @@ AFRAME.registerComponent('spawner', {
     var el = this.el;
     this.onHit = this.onHit.bind(this);
     this.spawn = this.spawn.bind(this);
+    this.onMouseDwon = this.onMouseDown.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
+    this.mousePressed = false;
+    this.mouseTimeStamp = null;
+    this.power = 0;
   },
 
   play: function() {
     var el = this.el;
-    el.addEventListener('collide', this.onHit);
+    //el.addEventListener('collide', this.onHit);
     el.addEventListener('spawn', this.spawn);
+    el.sceneEl.canvas.addEventListener('mouseDown', this.onMouseDown);
+    el.sceneEl.canvas.addEventListener('mouseUp', this.onMouseUp);
   },
 
   pause: function() {
     var el = this.el;
-    el.removeEventListener('collide', this.onHit);
+    //el.removeEventListener('collide', this.onHit);
     el.removeEventListener('spawn', this.spawn);
+    el.sceneEl.canvas.addEventListener('mouseDown', this.onMouseDown);
+    el.sceneEl.canvas.addEventListener('mouseUp', this.onMouseUp);
+
   },
 
+
+/*
   onHit: function(evt) {
     var hitEl = evt.detail.body.el;
     if(!hitEl.components.grab) {
@@ -55,10 +70,13 @@ AFRAME.registerComponent('spawner', {
     //  this.spawn();
     //}
     //console.log(evt.detail.body.el.id); */
+    /*
   },
+  */
 
   spawn: function() {
     var el = this.el;
+    /*
     var entity = document.createElement('a-entity');
     var matrixWorld = el.object3D.matrixWorld;
     var position = new THREE.Vector3();
@@ -69,7 +87,7 @@ AFRAME.registerComponent('spawner', {
     entity.setAttribute('position', position);
 
     entity.setAttribute('mixin', this.data.mixin);
-    entity.setAttribute('class', this.data.class)
+    entity.setAttribute('class', this.data.class);
     entity.addEventListener('loaded', function() {
       entityRotation = entity.getAttribute('rotation');
       entity.setAttribute('rotation', {
@@ -78,6 +96,53 @@ AFRAME.registerComponent('spawner', {
         z: entityRotation.z + rotation.z
       });
     });
-    el.sceneEl.appendChild(entity);
+    */
+    var poolName = 'pool__' + mixinName;
+    var entity = this.sceneEl.components[poolName].requestEntity();
+    //el.sceneEl.appendChild(entity);
+    var matrixWorld = el.object3D.matrixWorld;
+    var position = new THREE.Vector3();
+    var rotation = el.getAttribute('rotation');
+    var entityRotation;
+
+    position.setFromMatrixPosition(matrixWorld);
+    entity.setAttribute('position', position);
+    entity.setAttribute('mixin', this.data.mixin);
+    entity.setAttribute('class', this.data.class);
+    entity.addEventListener('loaded', function() {
+      entityRotation = entity.getAttribute('rotation');
+      entity.setAttribute('rotation', {
+        x: entityRotation.x + rotation.x,
+        y: entityRotation.y + rotation.y,
+        z: entityRotation.z + rotation.z
+      });
+    });
+    entity.play();
+    if(entity.body) {
+      entity.body.applyImpulse(
+        new CANNON.Vec3(
+          this.power*this.spawnVector.x,
+          this.power*this.spawnVector.y,
+          this.power*this.spawnVector.z
+        ),
+        new CANNON.Vec3().copy(entity.getComputedAttribute('position'))
+      );
+    }
   },
+
+  onMouseDown: function() {
+    var el = this.el;
+    if(!this.mouseDown) {
+      this.mouseTimeStamp = el.sceneEl.time;
+    }
+    this.mouseDown = true;
+  },
+
+  onMouseUp: function() {
+    var el = this.el;
+    this.mouseDown = false;
+    this.power = (el.sceneEl.time - this.mouseTimeStamp)/this.spawnChargeTime * spawnMagnitude;
+    if( this.power > this.spawnMagnitude) this.power = this.spawnMagnitude;
+    el.emit('spawn');
+  }
 });
---
+++ src/seedpods/ramshorn.js
@@ -0,0 +1,15 @@
+/* globals RAMSHORN */
+RAMSHORN.registerSeedPod(
+  //name
+  'ramsHorn'
+  //data
+  {
+    components: {
+      seedManager: {
+
+      },
+      poolSize: 10
+    },
+  }
+  //definition
+)
