6c7e1b12b49a903e64242e5a9518be757ff47dbd
vrplantsncsu <51127119+vrplantsncsu@users.noreply.github.com>
2019-10-10 13:51:26 -0400

    Rams Horn junk
    Working to bring back "spawners" from rift version and make them and the hand controllers only appear on 6DoF VR platforms

---
+++ src/components/6dof-only.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent('6dof-only', {
+  schema: {
+  },
+
+  init: function() {
+    var el = this.el;
+    el.sceneEl.addEventListener('loaded', handleSceneLoaded);
+    //el.sceneEl.addEventListener('exit-vr', handleEnterVR);
+  },
+
+  handleSceneLoaded: function(evt) {
+    var el = this.el;
+    if(!el.sceneEl.isMobile) {
+      el.visible = true;
+    } else {
+      el.visible = false;
+    }
+  },
+});
